ff8eb86fbee48e2316f76a2ceab8bc328e45df3d
Trevor Snyder <trevsny@gmail.com>
2018-08-09 08:48:11 -0700

    Added a new Item to db to save scraped data

--- apps/notams/models.py
+++ apps/notams/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import json
 
 from django.db import models
 
@@ -7,3 +8,20 @@ class Notam(models.Model):
     description = models.CharField(max_length = 255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+class ScrapyItem(models.Model):
+    unique_id = models.CharField(max_length=100, null=True)
+    data = models.TextField() #this is our crawled data
+    date = models.DateTimeField
+
+    # This is for basic and custom serialisation to return it to client as a JSON.
+    @property
+    def to_dict(self):
+        data = {
+            'data': json.loads(self.data),
+            'date': self.date
+        }
+        return data
+
+    def __str__(self):
+        return self.unique_id
